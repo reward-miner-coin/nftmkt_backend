997881e90afe0dde71b43b66e404d772de8759a4
Christian Martinez <christianbm1@gmail.com>
2022-06-06 20:05:06 -0400

    aded env variables for deno

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
+
 class Database {
   constructor() {
     this.connect();
@@ -7,13 +8,12 @@ class Database {
 
   async connect() {
    this.client = new Client({
-      user: "postgres",
-      database: "rmc",
-      hostname: "127.0.0.1",
-      password: "postgres",
-      port: 5432
+      user: Deno.env.get("DATABASE_USER") || "postgres",
+      database: Deno.env.get("DATABASE_NAME") || "rmc",
+      hostname: Deno.env.get("DATABASE_HOSTNAME") || "127.0.0.1",
+      password: Deno.env.get("DATABASE_PASSWORD") || "postgres",
+      port: Deno.env.get("DATABASE_PORT") || 5432
     });
-
     await this.client.connect();
   }
 }
