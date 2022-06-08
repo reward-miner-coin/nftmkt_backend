91cebbb9cce60743c9beb2ca4cd8f6277c00af76
Christian Martinez <christianbm1@gmail.com>
2022-06-08 16:05:24 -0400

    changed sql satements

--- db/create-bids-table.sql
+++ db/create-bids-table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS bids (
-    bidId UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  bidId UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 	mediaId text NOT NULL,
 	bidder VARCHAR(42) NOT NULL,
-    price double precision NOT NULL,
 	accepted bool DEFAULT false,
 	type bool NOT NULL,
+  	price double precision NOT NULL,
+	history bool NOT NULL default false,
 	created_at timestamp default current_timestamp
 )
