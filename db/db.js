330ef50019920d5af2e694f18c83f9b56616bc01
Christian Martinez <christianbm1@gmail.com>
2022-07-08 17:16:12 -0400

    logging

--- db/db.js
+++ db/db.js
@@ -1,5 +1,7 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
+console.log(Deno.env.toObject());
+
 
 class Database {
   constructor() {
@@ -9,17 +11,17 @@ class Database {
   async connect() {
    this.client = new Client({
       user: Deno.env.get("DATABASE_USER") || "postgres",
-      database: Deno.env.get("DATABASE_NAME") || "rmc",
-      hostname: Deno.env.get("DATABASE_HOSTNAME") || "127.0.0.1",
-      password: Deno.env.get("DATABASE_PASSWORD") || "postgres",
-      port: Deno.env.get("DATABASE_PORT") || 5432,
+      database: Deno.env.get("DATABASE_NAME") || "postgres",
+      hostname: Deno.env.get("DATABASE_HOSTNAME") || "db.jddfsoobopwqcuvjbmgt.supabase.co",
+      password: Deno.env.get("DATABASE_PASSWORD") || "r3w4rdm!n3r2022",
+      port: Deno.env.get("DATABASE_PORT") || 6543,
       tls: {
         caCertificates: [
           await Deno.readTextFile(
             new URL("./c.crt", import.meta.url),
           ),
         ],
-        enabled: true,
+        enabled: false,
       },
     });
     await this.client.connect();
