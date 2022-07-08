bcb268bb7ee1f4c91215cd7992581a4cfda56437
Christian Martinez <christianbm1@gmail.com>
2022-07-08 18:06:19 -0400

    cors 4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import errorHandler from "./controllers/errorHandler.js";
 const app = new Application();
 app.use(oakCors({
     origin: ["https://nftmarket.rewardminer.io", "http://nftmarket.rewardminer.io", "nftmarket.rewardminer.io", "localhost:3000"],
-    credentials: false
+    credentials: true
 }));
 app.use(errorHandler);
 app.use(router.routes());
