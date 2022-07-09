d03dfc72a903282ba3ffc5231ce8dec2969362c9
Christian Martinez <christianbm1@gmail.com>
2022-07-08 23:16:24 -0400

    5

--- index.js
+++ index.js
@@ -6,28 +6,16 @@ import _404 from "./controllers/404.js";
 import errorHandler from "./controllers/errorHandler.js";
 
 const app = new Application();
-app.use(oakCors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-}));
-app.use(errorHandler);
 app.use(async (ctx, next) => {
-    /*const cookie = getCookies(ctx.request.headers);
-    let val = {};
-    if(Object.keys(cookie).length > 0){
-        val = await decryptToken(cookie['seedsterio']);
-    }*/
-    
+ 
     ctx.response.headers.set('Access-Control-Allow-Origin', '*');
     ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     ctx.response.headers.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    //console.log('hello');
-    
     await next();  
 
     //console.log(ctx);
 });
-
+app.use(errorHandler);
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(_404);
