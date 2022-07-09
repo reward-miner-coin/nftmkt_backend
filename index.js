b12840adf7906650fb35d0b476b86df9f299f761
Christian Martinez <christianbm1@gmail.com>
2022-07-08 23:17:20 -0400

    5

--- index.js
+++ index.js
@@ -6,16 +6,7 @@ import _404 from "./controllers/404.js";
 import errorHandler from "./controllers/errorHandler.js";
 
 const app = new Application();
-app.use(async (ctx, next) => {
- 
-    ctx.response.headers.set('Access-Control-Allow-Origin', '*');
-    ctx.response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    ctx.response.headers.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    await next();  
 
-    //console.log(ctx);
-});
-app.use(errorHandler);
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(_404);
