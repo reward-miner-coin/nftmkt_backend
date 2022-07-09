5f128b94e4f16799f6cb8ac5ad353c47b60667e0
Christian Martinez <christianbm1@gmail.com>
2022-07-08 20:26:03 -0400

    added orgin *

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import _404 from "./controllers/404.js";
 import errorHandler from "./controllers/errorHandler.js";
 
 const app = new Application();
-app.use(oakCors());
+app.use(oakCors({
+    origin: "*"
+}));
 app.use(errorHandler);
 app.use(router.routes());
 app.use(router.allowedMethods());
