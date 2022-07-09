467236dadcc8cb89b8ce40667964414cc1bb88fc
Christian Martinez <christianbm1@gmail.com>
2022-07-09 01:29:56 -0400

    took out tcp/tls

--- controllers/collections/getallcollections.js
+++ controllers/collections/getallcollections.js
@@ -4,10 +4,8 @@ export default async ({ context, request, response }) => {
     const collections = await GetAllCollections();
 
     if(collections.length > 0){
-        
         //response.status = 200;
         response.body = { msg: `Collections retrieved`, data: collections, success: true};
-        console.log(response);
     } else {
         response.body = { msg: `Retrieving Collections error`, data: [], success: false};
     }
--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
-console.log(Deno.env.toObject());
-
+//console.log(Deno.env.toObject());
 
 class Database {
   constructor() {
@@ -15,10 +14,10 @@ class Database {
       hostname: Deno.env.get("DATABASE_HOSTNAME") || "127.0.0.1",
       password: Deno.env.get("DATABASE_PASSWORD") || "postgres",
       port: Deno.env.get("DATABASE_PORT") || 5432,
-      host_type: "tcp",
+      /*host_type: "tcp",
       tls: {
         enforce: false,
-      },
+      },*/
     });
     await this.client.connect();
   }
