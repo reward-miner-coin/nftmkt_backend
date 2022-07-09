26ebdb694071a687b6088c45bb03ea8825bb3d57
Christian Martinez <christianbm1@gmail.com>
2022-07-08 23:20:21 -0400

    55

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import _404 from "./controllers/404.js";
 import errorHandler from "./controllers/errorHandler.js";
 
 const app = new Application();
-
+app.use(errorHandler);
 app.use(oakCors({
     origin: false
 }));
@@ -20,7 +20,7 @@ app.use(async (ctx, next) => {
 
     //console.log(ctx);
 });
-app.use(errorHandler);
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(_404);
